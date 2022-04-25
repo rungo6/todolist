150c5a7c837706bdf3bccf2c36f61b6946dec0c5
rungo6 <t24331279@gmail.com>
2022-04-25 17:51:10 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -1,26 +1,29 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
+const exphbs = require('express-handlebars');
 
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
 
-mongoose.connect(process.env.MONGODB_URI,{
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    })
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
 
 const db = mongoose.connection
 
-db.on('error', () =>{
+db.on('error', () => {
   console.log('mongodb error')
 })
-db.once('open', () =>{
+db.once('open', () => {
   console.log('mongodb connected')
 })
 
-app.get('/',(req,res)=>{
-  res.send('Hello World')
+app.get('/', (req, res) => {
+  res.render('index')
 })
 
-app.listen(3000,()=>{
+app.listen(3000, () => {
   console.log('app is running on port 3000.')
 })
