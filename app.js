c66b767bc7dc45ffb5fd86cd4aa0bae0f3c5bfb4
rungo6 <t24331279@gmail.com>
2022-04-25 18:04:41 +0800

    feat: add creat todo function

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const mongoose = require('mongoose')
 const app = express()
 const exphbs = require('express-handlebars');
 const Todo = require('./models/todo')
+const bodyParser = require('body-parser')
 
+app.use(bodyParser.urlencoded({ extended: true }))
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
@@ -27,7 +29,15 @@ app.get('/', (req, res) => {
     .then(todos => res.render('index', { todos }))
     .catch(error => console.error(error))
 })
-
+app.get('/todos/new', (req, res) => {
+  return res.render('new')
+})
+app.post('/todos', (req, res) => {
+  const name = req.body.name
+  return Todo.create({ name })
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
 app.listen(3000, () => {
   console.log('app is running on port 3000.')
 })
