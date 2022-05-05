b91a51289f80322872b433adf7608cc9613d4a4c
rungo6 <t24331279@gmail.com>
2022-05-06 00:03:21 +0800

    feat add sort function

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ db.once('open', () => {
 app.get('/', (req, res) => {
   Todo.find()
     .lean()
+    .sort({ _id: 'asc' })
     .then(todos => res.render('index', { todos }))
     .catch(error => console.error(error))
 })
