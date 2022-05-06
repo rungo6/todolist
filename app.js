643b14119116d9f607fecade6bae21f8eb46105f
rungo6 <t24331279@gmail.com>
2022-05-06 09:09:37 +0800

    modify mongoose config

--- app.js
+++ app.js
@@ -1,76 +1,20 @@
 const express = require('express')
-const mongoose = require('mongoose')
-const app = express()
 const exphbs = require('express-handlebars');
-const Todo = require('./models/todo')
 const bodyParser = require('body-parser')
 const methOverride = require('method-override')
 const routes = require('./routes')
+require('./config/mongoose')
+
+const app = express()
+
 app.use(bodyParser.urlencoded({ extended: true }))
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(methOverride('_method'))
 
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-
-const db = mongoose.connection
-
-db.on('error', () => {
-  console.log('mongodb error')
-})
-db.once('open', () => {
-  console.log('mongodb connected')
-})
-
 app.use(routes)
 
-app.get('/todos/new', (req, res) => {
-  return res.render('new')
-})
-app.post('/todos', (req, res) => {
-  const name = req.body.name
-  return Todo.create({ name })
-    .then(() => res.redirect('/'))
-    .catch(error => console.log(error))
-})
 
-app.get("/todos/:id", (req, res) => {
-  const id = req.params.id
-  return Todo.findById(id)
-    .lean()
-    .then(todo => res.render('detail', { todo }))
-    .catch(error => console.log(error))
-})
-
-app.get('/todos/:id/edit', (req, res) => {
-  const id = req.params.id
-  return Todo.findById(id)
-    .lean()
-    .then((todo) => res.render('edit', { todo }))
-    .catch(error => console.log(error))
-})
-app.put('/todos/:id/', (req, res) => {
-  const id = req.params.id
-  const { name, isDone } = req.body
-  return Todo.findById(id)
-    .then(todo => {
-      todo.name = name
-      todo.isDone = isDone === 'on'
-      return todo.save()
-    })
-    .then(() => res.redirect(`/todos/${id}`))
-    .catch(error => console.log(error))
-})
-app.delete('/todos/:id', (req, res) => {
-  const id = req.params.id
-  return Todo.findById(id)
-    .then(todo => todo.remove())
-    .then(() => res.redirect('/'))
-    .catch(error => console.log(error))
-})
 app.listen(3000, () => {
   console.log('app is running on port 3000.')
 })
