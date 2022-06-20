9908137f699188a0a88ee059a0831fbbcd84617c
rungo6 <t24331279@gmail.com>
2022-06-20 09:34:44 +0800

    feat: add relations between User and Todo models

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose')
 
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
+  useCreateIndex: true
 })
 
 const db = mongoose.connection
--- routes/modules/home.js
+++ routes/modules/home.js
@@ -5,7 +5,8 @@ const router = express.Router()
 const Todo = require('../../models/todo')
 
 router.get('/', (req, res) => {
-  Todo.find()
+  const userId = req.user._id
+  Todo.find({ userId })
     .lean()
     .sort({ _id: 'asc' })
     .then(todos => res.render('index', { todos }))
